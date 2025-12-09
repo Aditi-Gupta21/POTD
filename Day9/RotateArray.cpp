2efe97acfcfcc37afedb533ca92bdf2ac77648a4
Aditi Gupta <aditigupta.21apr@gmail.com>
2025-12-09 22:31:42 +0530

    Day9 - Only 1 solved

---
+++ Day9/RotateArray.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        vector<int>temp; int n = nums.size();
+        k = k%n;
+        if(k == 0){
+            return;
+        }
+        for(int i = n-k;i<n;i++){
+            temp.push_back(nums[i]);
+        }
+        for(int i =0;i<n-k;i++){
+            temp.push_back(nums[i]);
+        }
+
+        for(int i =0;i<n;i++){
+            nums[i]=temp[i];
+        }
+    }
+};
+
+int main(){
+
+    return 0;
+}
